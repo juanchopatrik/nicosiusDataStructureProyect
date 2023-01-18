2ce0a6577f4966f37fdbf5b6659ac0d5c29b9676
juansilva <juandsr@hotmail.com>
2023-01-17 20:15:20 -0500

    Add project files.

---
+++ nicosiusDataStructureProyect/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nicosiusDataStructureProyect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*            double[] calif = new double[3];
+                        double sumatori = 0;
+                        double difference = 0;
+
+                        for (int i = 0; i < calif.Length; i++)
+                        {
+                            Console.WriteLine("Escribir Calificacion");
+                            calif[i] = Convert.ToDouble(Console.ReadLine());
+                            sumatori += calif[i];
+                        }
+            */
+/*            bool boolean = false;
+            char characterProobe = 'a';
+            string chaine = "Priviet";
+
+            switch (boolean)
+            {
+                case true:
+                    Console.WriteLine("is true");
+                    break;
+                case false:
+                    Console.WriteLine("is false");
+                    break ;
+            }
+
+
+            switch (characterProobe)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    Console.WriteLine("its a  voca");
+                    break;
+                default:
+                    Console.WriteLine("Is not a vocal");
+                    break;
+            }
+
+            switch (chaine)
+            {
+                case "hola":
+                    Console.WriteLine("Espanol");
+                    break;
+                case "hello":
+                    Console.WriteLine("English");
+                    break;
+                case "Priviet":
+                    Console.WriteLine("Ruso");
+                    break;
+            }
+*/      
+                   
+        }
+        
+    }
+}
