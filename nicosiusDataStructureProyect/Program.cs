6f20aca9ae5161c87c10400ad9da3a7cbea2297d
juansilva <juandsr@hotmail.com>
2023-02-04 12:00:34 -0500

    using out

--- nicosiusDataStructureProyect/Program.cs
+++ nicosiusDataStructureProyect/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,50 +23,66 @@ namespace nicosiusDataStructureProyect
                             sumatori += calif[i];
                         }
             */
-/*            bool boolean = false;
-            char characterProobe = 'a';
-            string chaine = "Priviet";
+            /*            bool boolean = false;
+                        char characterProobe = 'a';
+                        string chaine = "Priviet";
 
-            switch (boolean)
-            {
-                case true:
-                    Console.WriteLine("is true");
-                    break;
-                case false:
-                    Console.WriteLine("is false");
-                    break ;
-            }
+                        switch (boolean)
+                        {
+                            case true:
+                                Console.WriteLine("is true");
+                                break;
+                            case false:
+                                Console.WriteLine("is false");
+                                break ;
+                        }
 
 
-            switch (characterProobe)
-            {
-                case 'a':
-                case 'e':
-                case 'i':
-                case 'o':
-                case 'u':
-                    Console.WriteLine("its a  voca");
-                    break;
-                default:
-                    Console.WriteLine("Is not a vocal");
-                    break;
-            }
+                        switch (characterProobe)
+                        {
+                            case 'a':
+                            case 'e':
+                            case 'i':
+                            case 'o':
+                            case 'u':
+                                Console.WriteLine("its a  voca");
+                                break;
+                            default:
+                                Console.WriteLine("Is not a vocal");
+                                break;
+                        }
 
-            switch (chaine)
-            {
-                case "hola":
-                    Console.WriteLine("Espanol");
-                    break;
-                case "hello":
-                    Console.WriteLine("English");
-                    break;
-                case "Priviet":
-                    Console.WriteLine("Ruso");
-                    break;
-            }
-*/      
-                   
+                        switch (chaine)
+                        {
+                            case "hola":
+                                Console.WriteLine("Espanol");
+                                break;
+                            case "hello":
+                                Console.WriteLine("English");
+                                break;
+                            case "Priviet":
+                                Console.WriteLine("Ruso");
+                                break;
+                        }
+            */
+            int cantidad = 8;
+            int sumatoria = 0;
+            int factorial = 0;
+
+            Calculador(cantidad, out sumatoria, out factorial);
+            Console.WriteLine("Sumatoria = {0}, factorial = {1}", sumatoria, factorial);
         }
-        
+
+        private static void Calculador(int cantidad, out int sumatoria, out int factorial)
+        {
+            sumatoria = 0;
+            factorial = 1;
+
+            for (int m = 1; m < cantidad; m++)
+            {
+                sumatoria = sumatoria + m;
+                factorial = factorial * m;
+            }
+        }
     }
 }
