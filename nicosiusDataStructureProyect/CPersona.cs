2751e83bf9fd718a7b597cf113fa6056bb2c523d
juansilva <juandsr@hotmail.com>
2023-02-07 12:29:15 -0500

    overwriting object methods

---
+++ nicosiusDataStructureProyect/CPersona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nicosiusDataStructureProyect
+{
+    public class CPersona
+    {
+        private string nombre;
+        private int edad;
+
+        public CPersona(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Nombre: {0}, edad : {1}", nombre, edad);
+
+            return sb.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null && obj is CPersona)
+            {
+                CPersona tem = (CPersona)obj;
+                if (nombre == tem.nombre && edad == tem.edad)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
--- nicosiusDataStructureProyect/Program.cs
+++ nicosiusDataStructureProyect/Program.cs
@@ -11,117 +11,245 @@ namespace nicosiusDataStructureProyect
 {
     internal class Program
     {
+        public enum tipoImagen
+        {
+            BMP,
+            JPG,
+            PNG,
+            GIF,
+            TIFF
+        }
         static void Main(string[] args)
         {
-        // Boxing and unboxing
 
-        ArrayList miArray = new ArrayList();
+            //Entendiendo la clase Object
 
-            miArray.Add(5);
-            miArray.Add(6);
+            CPersona juan = new CPersona("Juan", 27);
 
-        //this line will throw a error
+            Console.WriteLine("To String");
+            Console.WriteLine(juan.ToString());
 
-        /*int result = miArray[0] + miArray[1];*/
+            Console.WriteLine("Get Type");
+            Console.WriteLine(juan.GetType().BaseType);
 
-        int result = (int)miArray[0] + (int)miArray[1]; 
-        Console.WriteLine("the result is {0}", result);
+            CPersona Maria = new CPersona("Maria", 20);
 
-            /*            double[] calif = new double[3];
-                        double sumatori = 0;
-                        double difference = 0;
+            //Vemos el comportamiento de equals
 
-                        for (int i = 0; i < calif.Length; i++)
-                        {
-                            Console.WriteLine("Escribir Calificacion");
-                            calif[i] = Convert.ToDouble(Console.ReadLine());
-                            sumatori += calif[i];
-                        }
-            */
-            /*            bool boolean = false;
-                        char characterProobe = 'a';
-                        string chaine = "Priviet";
+            Console.WriteLine("------- Equals ------------");
 
-                        switch (boolean)
-                        {
-                            case true:
-                                Console.WriteLine("is true");
-                                break;
-                            case false:
-                                Console.WriteLine("is false");
-                                break ;
-                        }
-
-
-                        switch (characterProobe)
-                        {
-                            case 'a':
-                            case 'e':
-                            case 'i':
-                            case 'o':
-                            case 'u':
-                                Console.WriteLine("its a  voca");
-                                break;
-                            default:
-                                Console.WriteLine("Is not a vocal");
-                                break;
-                        }
-
-                        switch (chaine)
-                        {
-                            case "hola":
-                                Console.WriteLine("Espanol");
-                                break;
-                            case "hello":
-                                Console.WriteLine("English");
-                                break;
-                            case "Priviet":
-                                Console.WriteLine("Ruso");
-                                break;
-                        }
-            */
-            //int cantidad = 8;
-            //int sumatoria = 0;
-            //int factorial = 0;
-            ////Out obliga a asignar almenos 1 ves valores de la variable dentro del metodo
-            //Calculador(cantidad, out sumatoria, out factorial);
-            //Console.WriteLine("Sumatoria = {0}, factorial = {1}", sumatoria, factorial);
-
-            ////Calcular promedios con diferente numero de parametros
-            //double prome = 0;
-            //prome = Promedio(3.5, 6.7, 8.9);
-            //Console.WriteLine("The average is {0}", prome);
-            //prome = Promedio(8.0,8.7,8.8,10.0,9.0);
-            //Console.WriteLine("The average is {0}", prome);
-        }
-
-        //se pueden usar diferente numero de parametros pero siempre la entrada sera un array
-        private static double Promedio(params double[] valores)
-        {
-            double suma = 0.0;
-            double prom = 0.0;
-            int n = 0;
-
-            for (int i = 0; i < valores.Length; i++)
+            if (juan.Equals(Maria))
             {
-                suma += valores[i];
+                Console.WriteLine("Son iguales");
+            }
+            else
+            {
+                Console.WriteLine("No son iguales");
+            }
+            CPersona Juan2 = new CPersona("Juan", 27);
+
+            //Vemos el comportamiento de equals
+
+            Console.WriteLine("------- Equals ------------");
+
+            if (juan.Equals(Juan2))
+            {
+                Console.WriteLine("Son iguales");
+            }
+            else
+            {
+                Console.WriteLine("No son iguales");
+
+
+
+
+
+                //Numeraciones
+                /*Son tipos creados por el programador
+                 * Son opciones finitas que sirven para no tener hardcode
+                 */
+
+                /*            string dato = "";
+                            double tamanoImg = 0.0;
+
+                            Console.WriteLine("Write the image zise: ");
+                            dato = Console.ReadLine();
+                            tamanoImg = Convert.ToDouble(dato);
+
+                            calcularEspacio(tipoImagen.PNG, tamanoImg);
+                */
+                // uso de foreach
+                /*El foreach es unas estructura de datos que se conecta con otra estructura de datos
+                 el foreach es quien elige como comunicarse con otras estructura
+                * tiene dos interface hasNext = bool establece si tiene un dato siguiente
+                * Next = devuelve el dato
+                * El foreach se acopla segun la estructura que quiere iterar
+                * Cuando se usa el foreach se usa unboxing
+                 */
+
+                //    //arreglos tipoi jagged
+
+                //    int[][] numbers = new int[4][];//cuatro reglones
+
+                //    //instanciar cada reglon
+                //    numbers[0] = new int[3];
+                //    numbers[1] = new int[4];
+                //    numbers[2] = new int[2];
+                //    numbers[3] = new int[3];
+
+                ////variables
+
+                //    int n = 0;
+                //    int m = 0;
+                //    string dato = "";
+
+                //    for ( n = 0; n < 4; n++)
+                //    {
+                //        for ( m = 0; m < numbers[n].Length; m++)
+                //        {
+                //            Console.WriteLine("escribe el numero {0} de la region {1}", m, n);
+                //            dato = Console.ReadLine();
+                //            numbers[n][m] = Convert.ToInt32(dato);
+                //        }
+
+                //    }
+
+                // Boxing and unboxing
+
+                //ArrayList miArray = new ArrayList();
+
+                //    miArray.Add(5);
+                //    miArray.Add(6);
+
+                ////this line will throw a error
+
+                ///*int result = miArray[0] + miArray[1];*/
+
+                //int result = (int)miArray[0] + (int)miArray[1]; 
+                //Console.WriteLine("the result is {0}", result);
+
+                /*            double[] calif = new double[3];
+                            double sumatori = 0;
+                            double difference = 0;
+
+                            for (int i = 0; i < calif.Length; i++)
+                            {
+                                Console.WriteLine("Escribir Calificacion");
+                                calif[i] = Convert.ToDouble(Console.ReadLine());
+                                sumatori += calif[i];
+                            }
+                */
+                /*            bool boolean = false;
+                            char characterProobe = 'a';
+                            string chaine = "Priviet";
+
+                            switch (boolean)
+                            {
+                                case true:
+                                    Console.WriteLine("is true");
+                                    break;
+                                case false:
+                                    Console.WriteLine("is false");
+                                    break ;
+                            }
+
+
+                            switch (characterProobe)
+                            {
+                                case 'a':
+                                case 'e':
+                                case 'i':
+                                case 'o':
+                                case 'u':
+                                    Console.WriteLine("its a  voca");
+                                    break;
+                                default:
+                                    Console.WriteLine("Is not a vocal");
+                                    break;
+                            }
+
+                            switch (chaine)
+                            {
+                                case "hola":
+                                    Console.WriteLine("Espanol");
+                                    break;
+                                case "hello":
+                                    Console.WriteLine("English");
+                                    break;
+                                case "Priviet":
+                                    Console.WriteLine("Ruso");
+                                    break;
+                            }
+                */
+                //int cantidad = 8;
+                //int sumatoria = 0;
+                //int factorial = 0;
+                ////Out obliga a asignar almenos 1 ves valores de la variable dentro del metodo
+                //Calculador(cantidad, out sumatoria, out factorial);
+                //Console.WriteLine("Sumatoria = {0}, factorial = {1}", sumatoria, factorial);
+
+                ////Calcular promedios con diferente numero de parametros
+                //double prome = 0;
+                //prome = Promedio(3.5, 6.7, 8.9);
+                //Console.WriteLine("The average is {0}", prome);
+                //prome = Promedio(8.0,8.7,8.8,10.0,9.0);
+                //Console.WriteLine("The average is {0}", prome);
             }
 
-            prom = suma / valores.Length;
+            //    private static void calcularEspacio(tipoImagen pImagen, double tamanoImg)
+            //{
+            //    double espacio = 0.0;
 
-            return prom;
-        }
+            //    switch (pImagen)
+            //    {
+            //        case tipoImagen.BMP:
+            //            espacio = tamanoImg * 1.1;
+            //            break;
+            //        case tipoImagen.JPG:
+            //            espacio = tamanoImg * 0.8;
+            //            break;
+            //        case tipoImagen.PNG:
+            //            espacio = tamanoImg * 1.77;
+            //            break;
+            //        case tipoImagen.GIF:
+            //            espacio = tamanoImg * 1.5;
+            //            break;
+            //        case tipoImagen.TIFF:
+            //            espacio = tamanoImg * 0.97;
+            //            break;
 
-        private static void Calculador(int cantidad, out int sumatoria, out int factorial)
-        {
-            sumatoria = 0;
-            factorial = 1;
+            //    }
+            //    Console.WriteLine("The Final size is {0}", espacio);
+            //}
 
-            for (int m = 1; m < cantidad; m++)
-            {
-                sumatoria = sumatoria + m;
-                factorial = factorial * m;
-            }
+            ////se pueden usar diferente numero de parametros pero siempre la entrada sera un array
+            //private static double Promedio(params double[] valores)
+            //{
+            //    double suma = 0.0;
+            //    double prom = 0.0;
+            //    int n = 0;
+
+            //    for (int i = 0; i < valores.Length; i++)
+            //    {
+            //        suma += valores[i];
+            //    }
+
+            //    prom = suma / valores.Length;
+
+            //    return prom;
+            //}
+
+            //private static void Calculador(int cantidad, out int sumatoria, out int factorial)
+            //{
+            //    sumatoria = 0;
+            //    factorial = 1;
+
+            //    for (int m = 1; m < cantidad; m++)
+            //    {
+            //        sumatoria = sumatoria + m;
+            //        factorial = factorial * m;
+            //    }
+            //}
         }
     }
 }
